425e4d816a93e04905cae2bc3fa0b174732e4c34
Diego Carrion Portilla <dcportilla2003@gmail.com>
2024-07-03 07:16:08 -0500

    idk

---
+++ ServicioWeb/src/main/java/ec/edu/ups/ppw/investigacion/ServicioWeb/RestApplication.java
@@ -0,0 +1,9 @@
+package ec.edu.ups.ppw.investigacion.ServicioWeb;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+    // Clase vacía, Jakarta EE se encarga de la configuración
+}
